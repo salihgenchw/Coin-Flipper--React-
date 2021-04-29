2db43ab8b2c8d5a45e46b3d32a9ad6e93ce947a6
Salih Genc <salihgenc34@gmail.com>
2021-04-29 16:05:41 +0300

    Yazi Tura odevi tamamlandi.

--- src/components/Coin/Coin.js
+++ src/components/Coin/Coin.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./Coin.css";
-import CoinTura from "../../Assets/coin-head.gif";
-import CoinYazi from "../../Assets/coin-tail.gif";
+import CoinTura from "../../Assets/Coin-head-Usa.png";
+import CoinYazi from "../../Assets/coin-tail.usa.png";
 
 const Coin = (props) => {
   return (
     <div className="Coin-container">
       <div className={`Coin ${props.flipping ? "Coin-rotate" : ""}`}>
-        <img
-          src={CoinTura}
-          className={props.side === "yazi" ? "Coin-back" : "Coin-front"}
-        />
-        <img
-          src={CoinYazi}
-          className={props.side === "yazi" ? "Coin-front" : "Coin-back"}
-        />
+        <img src={CoinTura} alt="head" className={props.side === "yazi" ? "Coin-back" : "Coin-front"}/>
+        <img src={CoinYazi} alt="tail" className={props.side === "yazi" ? "Coin-front" : "Coin-back"}/>
       </div>
     </div>
   );
--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -10,10 +10,22 @@ class CoinFlipper extends Component {
     this.state = {
       side: "tura",
       flipping: false,
+      total:0,//toplam atisi tutmak icin total ekledim
+      tail:0,
+      head:0
     };
   }
   handleClick = () => {
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
+    let tahmin = Math.round(Math.random()); // Random foksiyonu ile 1 ve 0 arasinda random sayi uretip round foksiyonuyla onu yakin tam sayiya esitliyoruz.Buradan bize 1 veya 0 donuyor random olarak.
+    
+    this.setState({total : this.state.total +1})
+    //her atis yapildiginda totali bir arttirdim.
+    console.log(tahmin);
+
+    tahmin ? this.setState({ side: "yazi", tail : this.state.tail + 1 }) : this.setState({ side: "tura" , head : this.state.head + 1 })
+    // burada tahmin true ise diyoruz yani 1 ise state'i yazi olarak degistirip tail sayacina 1 ekliyoruz.0'sa yani false'sa tura olarak degistiriyoruz ve head sayacina 1 ekliyoruz. tek sartli karar yapisi oldugu icin ternary operatoru kullandim.
+
     this.setState({ flipping: true });
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
     setTimeout(() => this.setState({ flipping: false }), 1000);
@@ -26,11 +38,11 @@ class CoinFlipper extends Component {
         <Coin side={this.state.side} flipping={this.state.flipping} />
         <button onClick={this.handleClick}>Try!</button>
         <p>
-          Toplam
-          <strong> 5 </strong>
+          Toplam {" "}
+          <strong>{this.state.total}{" "}</strong>
           atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
+          <strong> {this.state.head}{" "}</strong>ü tura
+          <strong> {this.state.tail}{" "}</strong>
           si yazı geldi.
         </p>
       </div>
